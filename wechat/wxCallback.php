43b04bf4bb30b0d627c2d8f56388fead9427c6c1
JinHM <986394140@qq.com>
2017-06-30 18:16:59 +0800

    补充之前上传的一些代码。

--- wechat/wxCallback.php
+++ wechat/wxCallback.php
@@ -3,10 +3,11 @@
 include_once "wxApi.php";
 include_once "crypto/wxBizMsgCrypt.php";
 
+// FIXME: 下面的参数从数据库的setting表中读取
 define("APP_ID", "");
 define("APP_SECRET", "");
 define("TOKEN", "");
-define("ENCODING_AES_KEY", "")
+define("ENCODING_AES_KEY", "");
 
 $wxObj = new WXCallback();
 if(!empty($_GET['echostr'])) {
@@ -14,8 +15,7 @@ if(!empty($_GET['echostr'])) {
 }
 
 
-class WXCallback
-{
+class WXCallback {
     /**
      * 微信验证，一般在配置服务器url时使用
      * @param $signature 微信加密签名，signature结合了开发者填写的token参数和请求中
@@ -37,7 +37,7 @@ class WXCallback
      * 检验signature对请求进行校验。若确认此次GET请求来自微信服务器，请原样返回
      * echostr参数内容，则接入生效，成为开发者成功，否则接入失败。
      */
-    private function checkSignature($signature, $timestamp, $nonce){
+    private function checkSignature($signature, $timestamp, $nonce) {
         $token = TOKEN;
 
         // 将token、timestamp、nonce三个参数进行字典序排序
