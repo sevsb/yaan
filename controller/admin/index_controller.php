bc800132eb4196247e38a60324199821bd9b2e2c
樱凌准 <SakuraSinojun@gmail.com>
2017-07-02 18:44:08 +0800

    update paths.
    Change-Id: Ia781167cd75420a77190f1d5e888b1cb379680c8

--- controller/admin/index_controller.php
+++ controller/admin/index_controller.php
@@ -1,6 +1,6 @@
 <?php
-include_once(dirname(__FILE__) . "/../app/config.php");
-include_once(dirname(__FILE__) . "/../app/login.class.php");
+include_once(dirname(__FILE__) . "/../..//config.php");
+include_once(dirname(__FILE__) . "/../../app/login.class.php");
 
 class index_controller {
     public function preaction($action) {
---
+++ controller/admin_controller.php
@@ -0,0 +1,21 @@
+<?php
+include_once(dirname(__FILE__) . "/../config.php");
+
+class admin_controller {
+    public function index_action() {
+        go("admin/index/index");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
