81215d58a793812d3e9334057e8c9fa92d0428fd
JinHM <986394140@qq.com>
2017-07-05 18:41:18 +0800

    添加 wechat/index/home 页面

---
+++ controller/wechat/index_controller.php
@@ -0,0 +1,12 @@
+<?php
+include_once(dirname(__FILE__) . "/../../config.php");
+include_once(dirname(__FILE__) . "/../../app/login.class.php");
+
+class index_controller {
+    public function home_action() {
+        $tpl = new tpl("wechat/header", "wechat/footer");
+        $tpl->display("wechat/index/home");
+    }
+}
+
+?>
