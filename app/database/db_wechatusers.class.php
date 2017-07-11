63edf26a149cab6de18878d69f76a3ca0085f8d2
樱凌准 <SakuraSinojun@gmail.com>
2017-07-11 16:37:27 +0800

    fix wechat login.
    Change-Id: I6e362af4374900f949890bb95c8af89acbe6ef5f

--- app/database/db_wechatusers.class.php
+++ app/database/db_wechatusers.class.php
@@ -39,29 +39,21 @@ class db_wechatusers extends database {
 
 
     public function add($openid, $nickname, $faceurl) {
-        $openid = $this->escape($openid);
-        $nickname = $this->escape($nickname);
-        $faceurl = $this->escape($faceurl);
         return $this->insert(TABLE_WECHATUSERS, array("openid" => $openid, "nickname" => $nickname, "face" => $faceurl));
     }
 
     public function update_location_by_id($userid, $location) {
         $userid = (int)$userid;
-        $location = $this->escape($location);
         return $this->update(TABLE_WECHATUSERS, array("location" => $location), "id = $userid");
     }
 
     public function update_location_by_openid($openid, $location) {
-        $location = $this->escape($location);
         $openid = $this->escape($openid);
         return $this->update(TABLE_WECHATUSERS, array("location" => $location), "openid = $openid");
     }
 
     public function update_profile($userid, $openid, $nickname, $faceurl) {
         $userid = (int)$userid;
-        $openid = $this->escape($openid);
-        $nickname = $this->escape($nickname);
-        $faceurl = $this->escape($faceurl);
         return $this->update(TABLE_WECHATUSERS, array("openid" => $openid, "nickname" => $nickname, "face" => $faceurl, "id = $userid"));
     }
 };
