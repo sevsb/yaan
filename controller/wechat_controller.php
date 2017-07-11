6b30849a7e8da3a6864cf9da794f4718592cb9b4
樱凌准 <SakuraSinojun@gmail.com>
2017-07-11 14:16:36 +0800

    fix wechat.
    Change-Id: I543e83e39e6e6c489d6862b87ac47a49240da4e9

---
+++ controller/wechat_controller.php
@@ -0,0 +1,50 @@
+<?php
+include_once(dirname(__FILE__) . "/../config.php");
+
+class wechat_controller {
+
+    public function index_action() {
+        $ret = WXApi::inst()->doOAuth(true);
+        if ($ret == null) {
+            die("认证失败。");
+        }
+        // $_SESSION["wechat"] = $ret;
+        $openid = $ret["openid"];
+        $user = db_wechatusers::inst()->get_user_by_openid($openid);
+        if (empty($user)) {
+            $nick = $ret["nickname"];
+            $faceurl = $ret["headimgurl"];
+            $id = db_wechatusers::inst()->add($openid, $nick, $faceurl);
+            $user = array("id" => $id, "openid" => $openid, "nickname" => $nick, "face" => $faceurl);
+        }
+        $_SESSION["user"] = $user;
+        $_SESSION["user.name"] = $user["nickname"];
+        go("wechat/index/home");
+    }
+
+    public function test_action() {
+        unset($_SESSION["user"]);
+        unset($_SESSION["user.name"]);
+
+        $userid = get_request_assert("user");
+        $user = db_wechatusers::inst()->get_user_by_id($userid);
+        logging::assert(!empty($user), "No such user.");
+        $_SESSION["user"] = $user;
+        $_SESSION["user.name"] = $user["nickname"];
+
+        go("wechat/index/home");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
