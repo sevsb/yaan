fc10d7e92b0f0653236598d82f9a07d2a2f283a9
JinHM <986394140@qq.com>
2017-07-11 17:54:04 +0800

    调整 wechat/index/sheet JSSDK功能

--- js/wechat/index/sheet.js
+++ js/wechat/index/sheet.js
@@ -1,19 +1,21 @@
 $(document).ready(function() {
-    // 点击底边栏的拍照功能，添加照片
-    $('.navbar_bottom').on('click', function() {
-        wx.chooseImage({
-            count: 1, // 默认9
-            sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
-            sourceType: ['camera'], // 可以指定来源是相册还是相机，默认二者都有
-            success: function (res) {
-                var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+    wx.ready(function () {
+        // 点击底边栏的拍照功能，添加照片
+        $('.navbar_bottom').on('click', function() {
+            wx.chooseImage({
+                count: 1, // 默认9
+                sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                sourceType: ['camera'], // 可以指定来源是相册还是相机，默认二者都有
+                success: function (res) {
+                    var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                }
+            });
+
+            var number = $('.card').length;
+            if(number < 10) {
+                addPhoto(number);
             }
         });
-
-        var number = $('.card').length;
-        if(number < 10) {
-            addPhoto(number);
-        }
     });
 });
 
