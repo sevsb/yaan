a5701e869f5cc2f5406380efd813afc8d81cf439
JinHM <986394140@qq.com>
2017-07-12 15:03:11 +0800

    尝试在 wechat/index/sheet 中完整使用wx.chooseImage的功能

--- js/wechat/index/sheet.js
+++ js/wechat/index/sheet.js
@@ -4,31 +4,28 @@ $(document).ready(function() {
         $('.navbar_bottom').on('click', function() {
             wx.chooseImage({
                 count: 1, // 默认9
-                sizeType: ['compressed'], // 可以指定是原图还是压缩图，默认二者都有
-                sourceType: ['camera'], // 可以指定来源是相册还是相机，默认二者都有
+                sizeType: ['original', 'compressed'], // ['original', 'compressed'] 可以指定是原图还是压缩图，默认二者都有
+                sourceType: ['camera'], // ['album', 'camera'] 可以指定来源是相册还是相机，默认二者都有
                 success: function (res) {
                     var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                    addPhoto(localIds);
                 }
             });
-
-            var number = $('.card').length;
-            if(number < 10) {
-                addPhoto(number);
-            }
         });
     });
 });
 
 /** 
  * 添加照片
- * @param json 应为从JS-SDK中拿到的相片数据和填写的备注
+ * @param data 应为从JS-SDK中拿到的相片数据和填写的备注
  *             因为目前是Demo，所以现在只是card元素的总和，为了添加预览的删除功能。
  */
-function addPhoto(json) {
-    var cardObject =  '<div class="card" id="card_'+json+'">';
+function addPhoto(data) {
+    var cardNumber = new Date();
+    var cardObject =  '<div class="card" id="card_'+cardNumber+'">';
         cardObject += '    <div class="card_body">';
         // TODO: 为了更好的显示竖版照片，应该在这里比较图片的长宽值，然后选择应该是max-height或者max-width
-        cardObject += '        <img class="card_img" src="/bigsword/yaan/images/default_images/0'+json+'.jpg">';
+        cardObject += '        <img class="card_img" src="'+data+'">';
         cardObject += '    </div>';
         cardObject += '    <div class="card_footer dropup" >';
         cardObject += '        <div class="card_title">这放八个字比较好...</div>';
@@ -37,7 +34,7 @@ function addPhoto(json) {
         cardObject += '        </button>';
         cardObject += '        <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dLabel" style="min-width: 0;">';
         cardObject += '            <li><a href="#">修改</a></li>';
-        cardObject += '            <li class="delete_btn" number="'+json+'"><a href="###">删除</a></li>';
+        cardObject += '            <li class="delete_btn" number="'+cardNumber+'"><a href="###">删除</a></li>';
         cardObject += '        </ul>';
         cardObject += '    </div>';
         cardObject += '</div>';
