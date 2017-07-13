35f07444b17ecdbed47e334d047ea5c9c1ad2fc4
JinHM <986394140@qq.com>
2017-07-14 00:49:41 +0800

    尝试为 wechat/index/sheet 添加了一些图像上传的代码

--- controller/wechat/index_controller.php
+++ controller/wechat/index_controller.php
@@ -25,6 +25,28 @@ class index_controller {
         $tpl->set('signPackage', $signPackage);
         $tpl->display("wechat/index/sheet");
     }
+
+    public function add_photo_ajax() {
+        $photo = get_request('photo');
+
+        $photoName = null;
+
+        if (substr($photo, 0, 5) == "data:") {
+            $ret = uploadImageViaFileReader($photo, function($photoName) {
+                return $photoName;
+            });
+            logging::e("uploadImage-ret", $ret);
+            if (strncmp($ret, "fail|", 5) == 0) {
+                return $ret;
+            }
+            $photoName = $ret;
+        }else {
+            $photoName = explode('/', $photo);
+            $photoName = end($photoName);
+        }
+
+        return $photoName;
+    }
 }
 
 ?>
--- js/wechat/index/sheet.js
+++ js/wechat/index/sheet.js
@@ -1,20 +1,40 @@
 $(document).ready(function() {
     wx.ready(function () {
+        var data = new Array();
         // 点击底边栏的拍照功能，添加照片
-        $('.#add_img').on('click', function() {
+        $('#add_img').on('click', function() {
             wx.chooseImage({
                 count: 1, // 默认9
                 sizeType: ['original'], // ['original', 'compressed'] 可以指定是原图还是压缩图，默认二者都有
                 sourceType: ['camera'], // ['album', 'camera'] 可以指定来源是相册还是相机，默认二者都有
                 success: function (res) {
-                    var localId = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-                    var data = new Array();
+                    data['localId'] = res.localIds[0]; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
                     data['cardNumber'] = new Date().getTime();
-                    data['localId'] = localId;
-                    addPhoto(data);
+                    // Android 获取的DATA是没有‘data:image/jpeg;base64,’前缀的，iOS 获取的 MIME type 为‘image/jpg’
+                    // 而且菊花的MATE7在 alert 后会死机，需要考虑是否加上 window.__wxjs_is_wkwebview 的判断？
+                    wx.getLocalImgData({
+                        localId: data['localId'],
+                        success: function (res) {
+                            var localData = res.localData;
+                            alert(localData);
+                        }
+                    });
+                    // wx.getLocation({
+                    //     type: 'wgs84', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'gcj02'
+                    //     success: function (res) {
+                    //         data['Location']= res.latitude+', '+res.longitude+', '+res.accuracy;
+                    //     }
+                    // });
+                    // // 初始化模态框中元素
+                    // // img元素中iPhone目前是可以正常显示的
+                    // $('#add_photo_modal_img').attr('src', data['localId']);
+                    // $('#add_photo_modal_img_content').val('');
+                    // $('#add_photo_modal').modal('show');
+                    // $('#add_photo_submit').on('click', function() {
+                    //     alert('upload img!');
+                    // })
                 }
             });
-            $('#add_photo_modal').modal('show');
         });
     });
 });
@@ -25,8 +45,6 @@ $(document).ready(function() {
  *             因为目前是Demo，所以现在只是card元素的总和，为了添加预览的删除功能。
  */
 function addPhoto(data) {
-    alert(data['cardNumber']);
-    alert(data['localId']);
     // 制作卡片元素
     var cardObject =  '<div class="card" id="card_'+data['cardNumber']+'">';
         cardObject += '    <div class="card_body" style="background:url('+data['localId']+') no-repeat center; background-size: cover;">';
