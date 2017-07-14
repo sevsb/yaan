68356e616925f8d19c267d1650f5cd7dce55dd42
樱凌准 <SakuraSinojun@gmail.com>
2017-07-14 14:59:37 +0800

    show answer maps.
    Change-Id: I65ec88931b4d11d3f2de91a00b03a2755db8903d

--- js/admin/sheet/index.js
+++ js/admin/sheet/index.js
@@ -17,7 +17,24 @@ $(document).ready(function() {
     });
 
     __request("admin.sheet.sheetlist", { }, function(data) {
-        console.debug(data);
+
+        var mapurl = "http://api.map.baidu.com/staticimage/v2?ak=fqiHuU0wMsAxwzQfphk0PvLdrLB3flrZ&mcode=666666&center=LONGITUDE,LATITUDE&width=300&height=200&zoom=13&markers=LONGITUDE,LATITUDE";
+        for (var k in data.data) {
+            for (var k1 in data.data[k].answers[0].reply.data) {
+                var latitude1 = data.data[k].answers[0].reply.data[k1].uploadloc.latitude;
+                var longitude1 = data.data[k].answers[0].reply.data[k1].uploadloc.longitude;
+                data.data[k].answers[0].reply.data[k1].uploadloc.mapurl = mapurl.replace(new RegExp(/LONGITUDE/g), longitude1).replace(new RegExp(/LATITUDE/g), latitude1);
+
+                if (typeof(data.data[k].answers[0].reply.data[k1].exifloc.latitude) != "undefined") {
+                    var latitude2 = data.data[k].answers[0].reply.data[k1].exifloc.latitude;
+                    var longitude2 = data.data[k].answers[0].reply.data[k1].exifloc.longitude;
+                    data.data[k].answers[0].reply.data[k1].exifloc.mapurl = mapurl.replace(new RegExp(/LONGITUDE/g), longitude1).replace(new RegExp(/LATITUDE/g), latitude1);
+                } else {
+                    data.data[k].answers[0].reply.data[k1].exifloc.mapurl = "";
+                }
+            }
+        }
+        console.debug(data.data);
         sheetlist.sheetlist = data.data;
     });
 
