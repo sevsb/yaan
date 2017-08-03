1ead9c120404c32f50e5db828e8813b17eae5ad2
bloodtear <xiaoyu1120520@126.com>
2017-08-03 18:25:56 +0800

    fixed

--- app/tasks.class.php
+++ app/tasks.class.php
@@ -260,7 +260,7 @@ class tasks {
             $broadcast_compare_ret = false;
             logging::d('broadcast_areas', $broadcast_areas);
             if (!empty($broadcast_areas)) {
-                //$broadcast_areas = json_decode($broadcast_areas);
+                $broadcast_areas = json_decode($broadcast_areas, true);
                 foreach ($broadcast_areas as $area) {
                     logging::d('area', $area);
                     $area_obj = new location($area);
